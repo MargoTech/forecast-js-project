0f67f5dae14c0316838afa9d0d9d22e68ceba22f
MargoTech <margobartashevich@gmail.com>
2025-01-23 16:00:12 +0100

    correct JS

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const cityInput = document.getElementById("cityInput");
 const getWeatherBtn = document.getElementById("getWeatherBtn");
 const weatherResults = document.getElementById("weatherResults");
 
-getWeatherBtn.addEventListener("click" function() {
-    const weatherText = cityInput.value.trim();
-    if (weatherText) {
-        
-    }
-})
+getWeatherBtn.addEventListener("click", function () {
+  const cityName = cityInput.value.trim();
+  if (cityName === "") {
+    weatherResults.textContent = "Please, add your city!";
+  } else {
+    weatherResults.textContent = `You choose city: ${cityName}`;
+  }
+});
