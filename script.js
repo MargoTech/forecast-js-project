bfa68eb395850fe09cefaf6e0eb362f318e4d3f0
MargoTech <margobartashevich@gmail.com>
2025-01-23 15:43:52 +0100

    adding JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const cityInput = document.getElementById("cityInput");
+const getWeatherBtn = document.getElementById("getWeatherBtn");
+const weatherResults = document.getElementById("weatherResults");
+
+getWeatherBtn.addEventListener("click" function() {
+    const weatherText = cityInput.value.trim();
+    if (weatherText) {
+        
+    }
+})
