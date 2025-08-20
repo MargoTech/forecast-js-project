93098d40979dcaabf7da235498652c474f87a002
MargoTech <margobartashevich@gmail.com>
2025-08-20 21:14:36 +0200

    Working with vanilla js version of Forecast projec

--- script.js
+++ script.js
@@ -4,35 +4,48 @@ const weatherResults = document.getElementById("weatherResults");
 
 const apiKey = "c8d69cfc7d78b8bcc8fcb039afb7167a";
 
-getWeatherBtn.addEventListener("click", function () {
+getWeatherBtn.addEventListener("click", async () => {
   const cityName = cityInput.value.trim();
-  if (cityName === "") {
-    weatherResults.textContent = "Please, add your city!";
+  if (cityName) {
+    renderError("Please enter a  city!");
     return;
   }
 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-
-  fetch(apiUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("City is not founded");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      const temp = data.main.temp;
-      const description = data.weather[0].description;
-      const city = data.name;
-      const iconCode = data.weather[0].icon;
-      const message = `There is ${temp}°C, ${description} in ${city}.`;
-
-      weatherResults.innerHTML = `
-        <p>${message}</p>
-        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather icon">`;
-    })
-    .catch((error) => {
-      console.error(error);
-      weatherResults.textContent = "Failed to retrieve weather data";
-    });
+  try {
+    renderLoading();
+    const data = await fetchWeather(cityName);
+    renderWeather(data);
+  } catch (error) {
+    renderError(error.message);
+  }
 });
+
+async function fetchWeather(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  const response = await fetch(apiUrl);
+
+  if (!response.ok) {
+    throw new Error("City is not founded");
+  }
+
+  return response.json();
+}
+
+function renderWeather(data) {
+  const { temp } = data.main;
+  const { description, icon } = data.weather[0];
+  const { name } = data;
+
+  weatherResults.innerHTML = `
+        <p>There is ${temp}°C, ${description} in ${name}./p>
+        <img src="https://openweathermap.org/img/wn/${iconCode}.png" alt="Weather icon" />
+     `;
+}
+
+function renderError(message) {
+  weatherResults.innerHTML = `<p style="color: red;">${message}</p>`;
+}
+
+function renderLoading() {
+  weatherResults.innerHTML = `<p>Loading...</p>`;
+}
