4c1fc0ee4e14c8f64157014b49bc597933a83287
MargoTech <margobartashevich@gmail.com>
2025-08-29 12:00:00 +0200

    Commit for August 29

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -4,6 +4,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 
 export function useWeather() {
   const [data, setData] = useState(null);
+  const [forecast, setForecast] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [history, setHistory] = useState(
@@ -18,9 +19,19 @@ export function useWeather() {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       );
       if (!res.ok) throw new Error("City not found");
+      const current = await res.json();
+      setData(current);
 
-      const json = await res.json();
-      setData(json);
+      const resForecast = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const forecastJson = await resForecast.json();
+
+      const daily = forecastJson.list.filter((item) =>
+        item.dt_txt.includes("12:00:00")
+      );
+
+      setForecast(daily);
 
       const updatedHistory = [city, ...history.filter((c) => c !== city)].slice(
         0,
@@ -36,5 +47,5 @@ export function useWeather() {
     }
   };
 
-  return { data, loading, error, fetchWeather, history };
+  return { data, forecast, loading, error, fetchWeather, history };
 }
